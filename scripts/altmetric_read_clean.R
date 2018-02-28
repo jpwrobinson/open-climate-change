86937c6a6c6d376c680d3ea2be71b9d6da26bde1
James Robinson <jpwrobinson@gmail.com>
2018-02-28 15:55:10 +0000

    added script to start cleaning altmetric and adding OA variable

---
+++ scripts/altmetric_read_clean.R
@@ -0,0 +1,43 @@
+
+
+
+## Script for reading in raw almetric, attaching OA status, removing extra columns
+
+rm(list=ls())
+setwd("~/GitHub/open-climate-change/")
+setwd('/Users/robins64/Documents/git_repos/open-climate-change')
+
+
+library(here)
+
+alt<-read.csv('Data/altmetric/Altmetric_2018-02-26.csv')
+## drop columns we probably won't use
+alt$Authors.at.my.Institution<-NULL
+alt$Departments<-NULL
+alt$National.Clinical.Trial.ID<-NULL
+alt$SSRN<-NULL
+alt$URN<-NULL
+alt$URI<-NULL
+alt$PubMed.ID<-NULL
+alt$PubMedCentral.ID<-NULL
+alt$Handle.net.IDs<-NULL
+alt$ADS.Bibcode<-NULL
+alt$arXiv.ID<-NULL
+alt$RePEc.ID<-NULL
+alt$Peer.review.mentions<-NULL
+alt$Weibo.mentions<-NULL
+alt$LinkedIn.mentions<-NULL
+alt$Pinterest.mentions<-NULL
+alt$Q.A.mentions<-NULL
+alt$Video.mentions<-NULL
+alt$Syllabi.mentions<-NULL
+alt$Badge.URL<-NULL
+
+## scopus entries
+load("./Data/scopus_OA_climate_clean.Rdata")  ## scopus data filtered by Jimmy - journals with > 300 papers in last 10 years
+
+## add OA info to altmetric dataframe
+alt$OA<-scop$OA[match(alt$DOI,scop$DOI)]
+
+
+dim(alt[is.na(alt$OA),])
