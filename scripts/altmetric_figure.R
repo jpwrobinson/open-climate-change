677469b82a23d8bc2257921cf28ca8006366688b
James Robinson <jpwrobinson@gmail.com>
2018-03-10 11:35:07 +0000

    altmetric fig

--- scripts/altmetric_figure.R
+++ scripts/altmetric_figure.R
@@ -3,7 +3,16 @@
 rm(list=ls())
 setwd("~/GitHub/open-climate-change/")
 setwd('/Users/robins64/Documents/git_repos/open-climate-change')
-library(here); library(dplyr); library(tidyr); library(ggplot2); theme_set(theme_bw()); library(gridExtra); library(lme4)
+se<-function(x) sd(x)/sqrt(length(x))
+library(here); library(dplyr); library(tidyr); library(ggplot2); theme_set(theme_bw()); library(plotrix)
+
+add_label <- function(xfrac, yfrac, label, pos = 4, ...){
+  u <- par("usr")
+  x <- u[1] + xfrac * (u[2] - u[1])
+  y <- u[4] - yfrac * (u[4] - u[3])
+  text(x, y, label, pos = pos, ...)
+}
+
 
 load(file='Data/altmetric_OA_clean.Rdata')
 names(alt)
@@ -35,4 +44,66 @@ ratio<-alt %>% mutate(OA = ifelse(OA == TRUE, 'Open', 'Closed')) %>%
 ratio$Open[is.na(ratio$Open)]<-0
 ratio$ratio<-with(ratio, Open/Closed)
 
-ggplot(ratio, aes(SJRfac, Closed, col=source))+geom_point() + facet_wrap (~source)
+# ggplot(ratio, aes(SJRfac, Closed, col=source))+geom_point() + facet_wrap (~source, scales='free') + 
+# 		geom_point(aes(SJRfac, Open, col=source), shape=2)
+
+
+### gather data for base figure
+ratio<-alt %>% mutate(OA = ifelse(OA == TRUE, 'Open', 'Closed')) %>%
+	select(News.mentions, Policy.mentions, Twitter.mentions, OA, SJRfac) %>%
+	gather(source, mentions, -SJRfac, -OA) %>% 
+	group_by(SJRfac, OA, source) %>% 
+	summarise(mean=mean(mentions), se = se(mentions)) 
+	
+ratio$xlim<-ifelse(ratio$OA=='Open', as.numeric(ratio$SJRfac)+0.1, as.numeric(ratio$SJRfac)-0.1)
+
+## plot
+pdf(file='figures/X_fig_altmetric.pdf', height=2, width=8)
+layout(matrix(c(1,2,3), nrow=1))
+cols<-c('#d8b365', '#5ab4ac')
+cx.ax=0.9
+par(mar=c(2.5,4,2,0))
+## news
+with(ratio[ratio$source=='News.mentions' & ratio$OA=='Closed',], 
+		plotCI(xlim, mean, ui=mean+2*se, li=mean-2*se, pch=19,cex=1.5, sfrac=0, 
+			axes=F, xlim=c(0.75, 4.25), xlab='', ylab='',
+			scol=cols[1], col=cols[1]))
+with(ratio[ratio$source=='News.mentions' & ratio$OA=='Open',], 
+		plotCI(xlim, mean, ui=mean+2*se, li=mean-2*se, pch=19,cex=1.5, sfrac=0,
+			scol=cols[2], col=cols[2], add=TRUE))
+axis(1, at=c(1:4), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
+axis(2, cex.axis=cx.ax)
+add_label(0.01, 0.1, 'News', font=2, cex=1)
+mtext(2, text='Mean mentions', line=2.5, cex=0.8)
+
+par(mar=c(2.5,2,2,2))
+## policy
+with(ratio[ratio$source=='Policy.mentions' & ratio$OA=='Closed',], 
+		plotCI(xlim, mean, ui=mean+2*se, li=mean-2*se, pch=19,cex=1.5, sfrac=0, 
+			axes=F, xlim=c(0.75, 4.25), xlab='', ylab='', ylim=c(0,0.7),
+			scol=cols[1], col=cols[1]))
+with(ratio[ratio$source=='Policy.mentions' & ratio$OA=='Open',], 
+		plotCI(xlim, mean, ui=mean+2*se, li=mean-2*se, pch=19,cex=1.5, sfrac=0,
+			scol=cols[2], col=cols[2], add=TRUE))
+axis(1, at=c(1:4), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
+axis(2, cex.axis=cx.ax)
+add_label(0.01, 0.1, 'Policy', font=2, cex=1)
+
+par(mar=c(2.5,0,2,4))
+## twitter
+with(ratio[ratio$source=='Twitter.mentions' & ratio$OA=='Closed',], 
+		plotCI(xlim, mean, ui=mean+2*se, li=mean-2*se, pch=19,cex=1.5, sfrac=0, 
+			axes=F, xlim=c(0.75, 4.25), xlab='', ylab='', ylim=c(0,30),
+			scol=cols[1], col=cols[1]))
+with(ratio[ratio$source=='Twitter.mentions' & ratio$OA=='Open',], 
+		plotCI(xlim, mean, ui=mean+2*se, li=mean-2*se, pch=19,cex=1.5, sfrac=0,
+			scol=cols[2], col=cols[2], add=TRUE))
+axis(1, at=c(1:4), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
+axis(2, cex.axis=cx.ax)
+add_label(0.01, 0.1, 'Twitter', font=2, cex=1)
+
+par(xpd=T)
+legend('bottomright', legend=c('Open', 'Closed'), 
+	col=rev(cols), pch=19, bty='n', inset=c(-0.2, 0), cex=0.9)
+
+dev.off()
