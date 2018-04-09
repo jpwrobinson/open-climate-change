1bb7e3b2f833a5afeb7bc5ab5a26894c6c1593a4
James Robinson <jpwrobinson@gmail.com>
2018-04-09 16:54:52 +0100

    updated fig2, pub quality. log axes on citations

--- scripts/Figure2.R
+++ scripts/Figure2.R
@@ -28,6 +28,7 @@ pred2<-expand.grid(OA =unique(mod.dat$OA), jour.bin.scaled=0, Year = 2007, Sourc
 scopus.avg<-predict(fit5a, newdata=pred2, re.form = NA)
 
 
+
 ## load altmetric data
 load('Data/altmetric_glmer_fit.Rdata')
 
@@ -42,41 +43,49 @@ policy.avg<-predict(policy, newdata=pred2, re.form = NA, type='response')
 
 
 ## plot
-pdf(file='figures/Figure1.pdf', height=2, width=8)
+pdf(file='figures/Figure2.pdf', height=2, width=8)
 layout(matrix(c(1,2,3,4), nrow=1))
 cols<-c('#d8b365', '#5ab4ac')
 cx.ax=0.9
-par(mar=c(2.5,4,2,0), xpd=FALSE)
+
+par(oma = c(0.5,2,0,0), mgp=c(3,0.6,0))
+
+par(mar=c(1.5,1.5, 1.5, 1.5), xpd=FALSE)
 
 ## scopus
 with(pred[pred$OA==FALSE,], 
-		plotCI(xlim, 10^p, ui=10^p, li=10^p, pch=19,cex=1.5, sfrac=0, 
-			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0, 60),
+		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0, 
+			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(1, 1.75),
 			scol=cols[1], col=cols[1]))
 with(pred[pred$OA==TRUE,], 
-		plotCI(xlim, 10^p, ui=10^p, li=10^p, pch=19,cex=1.5, sfrac=0,
+		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0,
 			scol=cols[2], col=cols[2], add=TRUE))
-axis(1, at=unique(mod.dat$jour.bin.scaled), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
-axis(2, cex.axis=cx.ax)
-add_label(0.01, 0.1, 'Scopus', font=2, cex=1)
-mtext(2, text='Mean #', line=2.5, cex=0.8)
+axis(1, at=sort(unique(mod.dat$jour.bin.scaled)), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
+# axis(2, at=seq(1, 1.75, 0.05), labels=c(10^seq(1, 1.75, 0.05)), cex.axis=cx.ax)
+
+### log axes labels
+xBig = log10(c(seq(10, 25, 5), seq(25, 50, 5)))
+axis(2, at=c(1, log10(25), log10(50)), labels=c(10, 25, 50), cex.axis=cx.ax, tcl=0)
+axis(2, xBig , labels=rep("", length(xBig)), tcl=-0.4)
+add_label(0.01, 0.1, 'A) Citations', font=2, cex=0.9)
+mtext(2, text='Mean #', line=2, cex=0.8)
 
 clip(-10, max(ratio.plot$xlim), 0, 100)
-abline(h=10^scopus.avg[1], col=alpha(cols[1], 0.5), lty=2)
-abline(h=10^scopus.avg[2], col=alpha(cols[2], 0.5), lty=2)
+abline(h=scopus.avg[1], col=alpha(cols[1], 0.5), lty=2)
+abline(h=scopus.avg[2], col=alpha(cols[2], 0.5), lty=2)
 
-par(mar=c(2.5,2,2,2), xpd=FALSE)
+par(mar=c(1.5,1.5, 1.5, 1.5), xpd=FALSE)
 ## news
 with(ratio.plot[ratio.plot$source=='news' & ratio.plot$OA=='Closed',], 
 		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0, 
-			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0, 0.7),
+			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0, 0.5),
 			scol=cols[1], col=cols[1]))
 with(ratio.plot[ratio.plot$source=='news' & ratio.plot$OA=='Open',], 
 		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0,
 			scol=cols[2], col=cols[2], add=TRUE))
 axis(1, at=unique(ratio.plot$SJRfac.scaled), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
 axis(2, cex.axis=cx.ax)
-add_label(0.01, 0.1, 'News', font=2, cex=1)
+add_label(0.01, 0.1, 'B) News', font=2, cex=0.9)
 
 clip(-10, max(ratio.plot$xlim), 0, 100)
 abline(h=news.avg[1], col=alpha(cols[1], 0.5), lty=2)
@@ -86,18 +95,18 @@ abline(h=news.avg[2], col=alpha(cols[2], 0.5), lty=2)
 
 # mtext(2, text='Mean mentions', line=2.5, cex=0.8)
 
-par(mar=c(2.5,2,2,2), xpd=FALSE)
+par(mar=c(1.5,1.5, 1.5, 1.5), xpd=FALSE)
 ## policy
 with(ratio.plot[ratio.plot$source=='policy' & ratio.plot$OA=='Closed',], 
 		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0, 
-			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0,0.7),
+			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0.1,0.5),
 			scol=cols[1], col=cols[1]))
 with(ratio.plot[ratio.plot$source=='policy' & ratio.plot$OA=='Open',], 
 		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0,
 			scol=cols[2], col=cols[2], add=TRUE))
 axis(1, at=unique(ratio.plot$SJRfac.scaled), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
 axis(2, cex.axis=cx.ax)
-add_label(0.01, 0.1, 'Policy', font=2, cex=1)
+add_label(0.01, 0.1, 'C) Policy', font=2, cex=0.9)
 
 clip(-10, max(ratio.plot$xlim), 0, 100)
 abline(h=policy.avg[1], col=alpha(cols[1], 0.5), lty=2)
@@ -106,18 +115,18 @@ abline(h=policy.avg[2], col=alpha(cols[2], 0.5), lty=2)
 # text(max(ratio.plot$xlim)+0.1, policy.avg[1], label=round(policy.avg[1], 2), col='grey',cex=0.8)
 
 
-par(mar=c(2.5,1,2,3), xpd=FALSE)
+par(mar=c(1.5,1.5, 1.5, 1.5), xpd=FALSE)
 ## twitter
 with(ratio.plot[ratio.plot$source=='twitter' & ratio.plot$OA=='Closed',], 
 		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0, 
-			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0,10),
+			axes=F, xlim=c(-1.5, 1.5), xlab='', ylab='', ylim=c(0,5),
 			scol=cols[1], col=cols[1]))
 with(ratio.plot[ratio.plot$source=='twitter' & ratio.plot$OA=='Open',], 
 		plotCI(xlim, p, ui=p, li=p, pch=19,cex=1.5, sfrac=0,
 			scol=cols[2], col=cols[2], add=TRUE))
 axis(1, at=unique(ratio.plot$SJRfac.scaled), labels=c('Low', 'Medium', 'High', 'Very high'), cex.axis=cx.ax)
 axis(2, cex.axis=cx.ax)
-add_label(0.01, 0.1, 'Twitter', font=2, cex=1)
+add_label(0.01, 0.1, 'D) Twitter', font=2, cex=0.9)
 
 clip(-10, max(ratio.plot$xlim), 0, 100)
 abline(h=twitter.avg[1], col=alpha(cols[1], 0.5), lty=2)
@@ -128,6 +137,6 @@ abline(h=twitter.avg[2], col=alpha(cols[2], 0.5), lty=2)
 
 par(xpd=T)
 legend('topright', legend=c('Open', 'Closed'), 
-	col=rev(cols), pch=19, bty='n', inset=c(-0.2, 0), cex=0.9)
+	col=rev(cols), pch=19, bty='n', inset=c(-0.05, -0.12), cex=0.9)
 
 dev.off()
