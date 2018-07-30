935034a721caf1142579e446977ae4930e7846bf
James Robinson <jpwrobinson@gmail.com>
2018-07-30 11:26:30 -0700

    updated almetric database for 100 threshold. also cleaned

--- scripts/altmetric_read_clean.R
+++ scripts/altmetric_read_clean.R
@@ -11,7 +11,8 @@ setwd('/Users/robins64/Documents/git_repos/open-climate-change')
 library(here); library(stringr)
 
 # alt<-read.csv('Data/altmetric/Altmetric_2018-02-26.csv')
-alt<-read.csv('Data/altmetric/Altmetric_2018-03-04.csv')
+# alt<-read.csv('Data/altmetric/Altmetric_2018-03-04.csv')
+alt<-read.csv('Data/altmetric/Altmetric_2018-07-30.csv')
 
 ## drop columns we probably won't use
 alt$Authors.at.my.Institution<-NULL
@@ -51,7 +52,7 @@ load("./Data/scopus_OA_climate_clean.Rdata")  ## scopus data filtered by Jimmy -
 ## add OA info to altmetric dataframe
 alt$OA<-scop$OA[match(alt$DOI,scop$DOI)]
 dim(alt[is.na(alt$OA),])
-## missing 3728 papers: these are commentaries ?
+## missing 4463 papers: these are commentaries ?
 
 ## drop missing papers
 alt <- alt[!is.na(alt$OA),]
@@ -82,10 +83,27 @@ alt$Journal[alt$Journal=='Science of The Total Environment']<-'Science of the To
 alt$Journal[alt$Journal=='AMBIO']<-'Ambio'
 alt$Journal[alt$Journal=='Estuarine Coastal and Shelf Science']<-'Estuarine, Coastal and Shelf Science'
 
+## new journal fixes for 100 threshold
+alt$Journal[alt$Journal=="ICES Journal of Marine Science / Journal du Conseil"]<-'ICES Journal of Marine Science'
+alt$Journal[alt$Journal=="Renewable Energy: An International Journal"]<-'Renewable Energy'
+alt$Journal[alt$Journal=="Energies (19961073)"]<-'Energies'
+alt$Journal[alt$Journal=="Forests (19994907)"]<-'Forests'
+alt$Journal[alt$Journal=="Annals of Forest Science "]<-'Annals of Forest Science'
+alt$Journal[alt$Journal=="SCIENCE CHINA Earth Sciences"]<-'Science China Earth Sciences'
+alt$Journal[alt$Journal=="Agronomy for Sustainable Development (EDP Sciences)"]<-'Agronomy for Sustainable Development'
+alt$Journal[alt$Journal=="Natural Hazards and Earth System Sciences"]<-'Natural Hazards and Earth System Science'
+alt$Journal[alt$Journal=="Annals of Forest Science (EDP Sciences)"]<-'Annals of Forest Science'
+alt$Journal[alt$Journal=="Physics and Chemistry of the Earth -  Parts A/B/C"]<-'Physics and Chemistry of the Earth'
+alt$Journal[alt$Journal=="Trees: Structure and Function"]<-'Trees - Structure and Function'
+alt$Journal[alt$Journal=="Yingyong Shengtai Xuebao"]<-'Chinese Journal of Ecology'
+
 ## add impact bins
 bins<-read.csv('Data/BinJournallist_fromScopus.csv')
 alt$SJRfac<-bins$bin[match(alt$Journal, bins$Source.title)]
 
+
+unique(alt$Journal[is.na(alt$SJRfac)])
+# bins$Source.title[grepl('Chinese', bins$Source.title)]
 
 save(alt, file='Data/altmetric_OA_clean.Rdata')
 
